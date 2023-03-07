3a5dd2acade6ab1789797cc594cd8db9139aca25
Guillaume Pontanier <guillaume.pontanier@gmail.com>
2023-03-07 19:34:44 +0100

    modification ajout de la consigne ex03

--- ex03/ft_print_update_array.js
+++ ex03/ft_print_update_array.js
@@ -1,3 +1,5 @@
+/*Écrire une fonction qui reçoit un tableau de chiffre en paramètre, qui ajoute plus un à la valeur de chaque cellule et qui affiche le tableau mis à jour.*/
+
 function ft_print_update_array(array) {
     for (let index = 0; index < array.length; ++index) {
       array[index] = array[index]+1;
