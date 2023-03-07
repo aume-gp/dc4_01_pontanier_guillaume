fb3cdd8970bc35e960ba17dff691c01ecb16108f
Guillaume Pontanier <guillaume.pontanier@gmail.com>
2023-03-07 19:26:45 +0100

    modification nom fonction

--- ex02/ft_print_half_array.js
+++ ex02/ft_print_half_array.js
@@ -1,6 +1,6 @@
 /* Écrire une fonction qui affiche dans la console de développement une case sur deux d’un tableau de reçu en paramètre.*/
 
-  function printHalfArray(array) {
+  function ft_print_half_array(array) {
     for(let i = 0; i < array.length; i++) {
       if(i % 2 === 1) {
       console.log(array[i]);
@@ -8,4 +8,4 @@
     }
   }
   
-  printHalfArray([1,2,3,4,5,6,7,8,9,10])
+  ft_print_half_array([1,2,3,4,5,6,7,8,9,10])
